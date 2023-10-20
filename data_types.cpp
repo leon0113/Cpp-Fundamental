f7a9dc197c70c29b0170ed9082311d245da95ce6
Leon <tahjib.leon399@gmail.com>
2023-10-20 08:56:58 +0600

    data types & input

---
+++ data_types.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // variable declare
+    // int a = 10, b = 20, c = 30 
+//    int a;
+    // a = 10; // assign value
+    // cout<<a;
+
+    // char x = 'r';
+    // cout<<x;
+
+    // float f = 2.31;
+    // cout<<f;
+
+    // double d = 3.2432;
+    // cout<<d;
+
+    bool b = false;
+    cout<<b; // 0 output for false & 1 output for true
+
+}
--- first.cpp
+++ first.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(){
-    printf("Leon");
+    printf("Hello World");
     return 0;   
 }
