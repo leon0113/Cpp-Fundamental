915b3fa7304085d646c218caac39246e81576f69
Leon <tahjib.leon399@gmail.com>
2023-10-26 08:33:55 +0600

    type casting

---
+++ typeCasting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a = 69;
+  char c = 'b';
+  c=a;
+  cout<<c;
+}
--- userInput.cpp
+++ userInput.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main()
 {
     int a, b, c;
+    cout << "Enter your first number: ";
     cin>>a;
+    cout << "Enter your second number: ";
     cin>>b;
     c = a + b;
-    cout<<c;
+    cout<<"The sum of your two given numbers is: "<<c;
 }
