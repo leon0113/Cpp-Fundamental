1d1243fe7534c41fc8b09daf1e3460028355f112
Leon <tahjib.leon399@gmail.com>
2023-10-26 21:46:59 +0600

    practice

--- loop1.cpp
+++ loop1.cpp
@@ -11,3 +11,15 @@ int main(){
         }
     }
 }
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter your number: ";
+//     cin>>n;
+//     for(int i = 2; i <= n; i=i+2){
+//             cout<<i<<endl;
+//     }
+// }
