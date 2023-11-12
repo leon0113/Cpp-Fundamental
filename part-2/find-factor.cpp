0c8b65d100da4a9127bdc10b511ddd20ef345b52
Leon <tahjib.leon399@gmail.com>
2023-11-12 23:52:21 +0600

    find factors

---
+++ part-2/find-factor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 2, n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    while (i <= n-1)
+    {
+      if(n % i == 0){
+        cout<<i<<endl;
+      }
+      i++;
+    }
+    
+}
