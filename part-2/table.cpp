765bc519802431af786a13bb1a1193412b603f07
Leon <tahjib.leon399@gmail.com>
2023-11-12 23:39:33 +0600

    table program

---
+++ part-2/table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, untile, i =1;
+    cout<<"Enter the number: ";
+      cin>>n;
+
+    cout<<"Untile? : ";
+    cin>>untile;
+
+    while(i <= untile){
+      cout<< n* i<<endl;
+      i++;
+    }
+}
