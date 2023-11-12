66163257600b8243574e1a5a156f627c75ce1ecc
Leon <tahjib.leon399@gmail.com>
2023-11-13 00:26:27 +0600

    Even number 1 to n

---
+++ part-2/even-number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1, n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    while (i <= n)
+    {
+        if(i % 2 == 0){
+            cout << i << endl;
+    }
+    i++;
+    }
+    }
