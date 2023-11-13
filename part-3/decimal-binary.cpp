c34c303f93d308e559ee6a29285c4a51afac68fe
Leon <tahjib.leon399@gmail.com>
2023-11-14 00:05:33 +0600

    Decimal to Binary

---
+++ part-3/decimal-binary.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ans = 0, mul = 1, num, rem;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num > 0)
+    {
+        rem = num % 2; // find rem
+        num = num / 2; // update num
+        ans = rem * mul + ans;
+        // update mul
+        mul = mul * 10;
+    }
+    cout << ans;
+}
