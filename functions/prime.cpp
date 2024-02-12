cfa3256701060f7c2288cc3bc6ffa2d1b4820406
Leon <tahjib.leon399@gmail.com>
2024-02-12 18:42:42 +0600

    Prime number Fn

---
+++ functions/prime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+const char *primeOrNot(int n)
+{
+    if (n < 2)
+        return "No";
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return "No";
+    }
+    return "Yes";
+}
+
+int main()
+{
+    int x;
+    cout << "Enter the number: ";
+    cin >> x;
+    cout << "Result: " << primeOrNot(x);
+}
